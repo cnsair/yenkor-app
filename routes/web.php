b954b9572fb84f5020a6c1d58f119f14c2fa5788
Nwachukwu Chisom <samsondestined@gmail.com>
2024-10-13 18:40:40 +0100

    home route landing page

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
-    Route::get('/home', function () {
+    Route::get('/', function () {
         return view('home.home');
     })->name('home');
 
