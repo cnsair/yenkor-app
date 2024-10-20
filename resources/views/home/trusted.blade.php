9dbfd41267a98d20f4510d4a3629bb18c3822ab7
BismarkOdonkor <bikhay15@gmail.com>
2024-10-20 21:13:11 +0000

    I editted doownload , how it works , testimonial and trusted php files

--- resources/views/home/trusted.blade.php
+++ resources/views/home/trusted.blade.php
@@ -7,27 +7,33 @@
                         Trusted Cab Services <span class="d-block">in the World</span>
                     </h2>
                     <p>
-                        Curabitur placerat cursus nisi nec pharetra. Proin quis tortor
-                        fringilla, placerat nisi nec, auctor ex. Donec commodo orci ac
-                        lectus mattis, sed interdum sem scelerisque.
+                    Yenkor offers a globally trusted cab service designed to make your travel experience reliable, 
+                    efficient, and secure. With just two taps, you can easily book a ride and connect with professional 
+                    drivers near you, ensuring a quick and hassle-free experience. <br> Yenkor allows you to track your driver 
+                    in real-time, providing peace of mind by keeping you informed about their location and estimated time 
+                    of arrival. <br> Our priority is your safety, which is why we only work with trusted drivers who adhere 
+                    to strict safety standards. Whether you're heading to work, running errands, or traveling long 
+                    distances, Yenkor guarantees a seamless and secure journey, making us one of the most trusted 
+                    cab services worldwide. Wherever you are, YENKOR ensures you arrive at your destination safely 
+                    and comfortably.
                     </p>
                     <div class="list-group-flush mb-4">
                         <div class="list-group-item bg-transparent border-bottom-0 p-0">
                             <p class="mb-0">
                                 <i class="fas fa-check-circle mr-4 p-3 grey rounded-circle"
-                                    aria-hidden="true"></i>Cras justo odio
+                                    aria-hidden="true"></i>Smart Public Transportation
                             </p>
                         </div>
                         <div class="list-group-item bg-transparent border-bottom-0 p-0">
                             <p class="mb-0">
                                 <i class="fas fa-check-circle mr-4 mr-4 grey p-3 rounded-circle"
-                                    aria-hidden="true"></i>Dapibus ac facilisis in
+                                    aria-hidden="true"></i>Seamless Travel
                             </p>
                         </div>
                         <div class="list-group-item bg-transparent border-bottom-0 p-0">
                             <p class="mb-0">
                                 <i class="fas fa-check-circle mr-4 mr-4 grey p-3 rounded-circle"
-                                    aria-hidden="true"></i>Morbi leo risus
+                                    aria-hidden="true"></i>Digital Payments
                             </p>
                         </div>
                     </div>
